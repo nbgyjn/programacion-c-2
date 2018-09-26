f672e2d821f219e34e7608cda9be8aa24334b928
RAmirez hugo <36230599+nbgyjn@users.noreply.github.com>
2018-09-26 12:04:25 -0300

    tp para parcial

---
+++ clase 11(26-09)/repaso/abmEmpleadosSector/tp.h
@@ -0,0 +1,48 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldo;
+    int idSector;
+    int isEmpty;
+} eEmpleado;
+
+typedef struct
+{
+    int id;
+    char descripcion[20];
+} eSector;
+
+
+void inicializarEmpleados( eEmpleado x[], int tam);
+int buscarLibre( eEmpleado x[], int tam);
+int buscarEmpleado(eEmpleado x[], int tam, int legajo);
+void mostrarEmpleado(eEmpleado emp, eSector sectores[], int tamSector);
+void mostrarEmpleados(eEmpleado nomina[], int tam, eSector sectores[], int tamSector);
+void agregarEmpleado(eEmpleado empleados[], int tam, eSector sectores[], int tamSector);
+void eliminarEmpleado(eEmpleado *empleados, int tam, eSector sectores[], int tamSector);
+void modificarEmpleado(eEmpleado empleados[], int tam, eSector sectores[], int tamSector);;
+int elegirSector(eSector sectores[], int tam);
+void cargarDescripcion(eSector sectores[], int tamSector, int idSector, char cadena[]);
+void listarEmpleadosXSector(eEmpleado x[],int tam, eSector sectores[], int tamSector);
+void ordenarXSectorYNombre(eEmpleado x[],int tam, eSector sectores[], int tamSector);
+void mostrarEmpleadosMasGanadores(eEmpleado x[],int tam, eSector sectores[], int tamSector);
+void harcodearEmpleados(eEmpleado x[]);
+int menu();
+
+
+
+
+
+
+
+
+
+
+
+#endif // FUNCIONES_H_INCLUDED
